6d7aa3a52d12cb9f88fd67b92a2a529ebe3522cb
blue_p <113103389+blue-p-1214@users.noreply.github.com>
2024-03-29 18:00:59 +0900

    spring framework

---
+++ smarTravel/src/main/java/CD24/smarTravel/SmarTravelApplication.java
@@ -0,0 +1,13 @@
+package CD24.smarTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarTravelApplication.class, args);
+	}
+
+}
