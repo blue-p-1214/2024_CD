ba86ec84f0b042a95ef1fd7ebd321094db4a30ea
blue_p <113103389+blue-p-1214@users.noreply.github.com>
2024-05-06 17:40:02 +0900

    Delete smarTravel directory

--- smarTravel/src/main/java/com/CD2024/smarTravel/config/ApplicationConfig.java
+++
@@ -1,7 +0,0 @@
-package com.CD2024.smarTravel.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ApplicationConfig {
-}
--- smarTravel/src/main/webapp/WEB-INF/views/assets/js/home.js
+++
@@ -1,2 +0,0 @@
-console.log("home load");
-console.log("박종하 화이팅");
